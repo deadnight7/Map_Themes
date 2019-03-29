4a98309b3288c8a009f0aac30558b37b14a80843
Chirag Pandit <chirag.pandit@here.com>
2019-03-29 10:25:49 +0530

    Added Maps themes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+ 
+import SimpleHereMap from './Map.js';
+import ThemeSelector from './ThemeSelector.js';
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'normal.day',
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(evt) {
+    evt.preventDefault();
+
+    var change = evt.target.id;
+    this.setState({
+      "theme": change,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SimpleHereMap
+          app_id="hVA6vKr6ixW90KYvkb7k"
+          app_code="IulCKZdfTxT9b5NPveT_og"
+          lat="42.345978"
+          lng="-83.0405"
+          zoom="12"
+          theme={this.state.theme}
+        />
+        <ThemeSelector changeTheme={this.onChange} />
       </div>
     );
   }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <Map />
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
